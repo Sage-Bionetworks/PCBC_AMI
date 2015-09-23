58e8b4811d8ff048c48d7aecacfd44b8f678a7a9
kdaily <kdaily@users.noreply.github.com>
2015-09-23 10:31:06 -0700

    Updated to use github

--- altanalyze_provisioner.sh
+++ altanalyze_provisioner.sh
@@ -1,3 +1,5 @@
-wget https://altanalyze.googlecode.com/files/AltAnalyze_v.2.0.8.1-Ubuntu_12.04.zip -P /tmp
+# wget https://altanalyze.googlecode.com/files/AltAnalyze_v.2.0.8.1-Ubuntu_12.04.zip -P /tmp
+git clone https://github.com/nsalomonis/altanalyze.git
 
-unzip /tmp/AltAnalyze_v.2.0.8.1-Ubuntu_12.04.zip -D /usr/local/
+cd altanalyze
+
