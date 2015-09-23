32ed7c5a6ddee79aa2fdc1e698a99f0ad6c8bedb
kdaily <kdaily@users.noreply.github.com>
2015-09-23 10:58:51 -0700

    added comment

--- altanalyze_provisioner.sh
+++ altanalyze_provisioner.sh
@@ -2,5 +2,6 @@ git clone https://github.com/kdaily/altanalyze.git
 cd altanalyze
 git checkout bozeman
 
+# Download databases
 python AltAnalyze.py --update Official --version EnsMart72 --species Hs --platform RNASeq
 
