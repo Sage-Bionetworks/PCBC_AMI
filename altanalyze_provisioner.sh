fb30f35ebcecf1c7ea43de79cda71e9af062a43c
Kenneth Daily <kmdaily@gmail.com>
2015-09-26 02:18:31 +0000

    Added pcbc user install

--- altanalyze_provisioner.sh
+++ altanalyze_provisioner.sh
@@ -5,3 +5,13 @@ git checkout bozeman
 
 # Download databases
 python AltAnalyze.py --update Official --version EnsMart72 --species Hs --platform RNASeq
+
+cd /home/pcbc
+git clone https://github.com/kdaily/altanalyze.git
+chown -R pcbc:pcbc altanalyze
+
+cd altanalyze
+git checkout bozeman
+
+# Download databases
+python AltAnalyze.py --update Official --version EnsMart72 --species Hs --platform RNASeq
