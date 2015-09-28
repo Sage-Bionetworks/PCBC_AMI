8a36e694f1ff233e4dc39f1509ec2be714e55b20
Kenneth Daily <kmdaily@gmail.com>
2015-09-28 13:28:19 +0000

    cp kallisto

--- altanalyze_provisioner.sh
+++ altanalyze_provisioner.sh
@@ -1,17 +1,20 @@
-cd /opt
-git clone https://github.com/kdaily/altanalyze.git
-cd altanalyze
-git checkout bozeman
+# cd /opt/
+# git clone https://github.com/kdaily/altanalyze.git
+# cd altanalyze
+# git checkout bozeman
 
 # Download databases
-python AltAnalyze.py --update Official --version EnsMart72 --species Hs --platform RNASeq
+# python AltAnalyze.py --update Official --version EnsMart72 --species Hs --platform RNASeq
 
 cd /home/pcbc
 git clone https://github.com/kdaily/altanalyze.git
 chown -R pcbc:pcbc altanalyze
+chmod -R a+rwx altanalyze
 
 cd altanalyze
 git checkout bozeman
+
+cp /usr/local/bin/kallisto /home/pcbc/altanalyze/AltDatabase/kallisto/0.42.1/Linux/bin/
 
 # Download databases
 python AltAnalyze.py --update Official --version EnsMart72 --species Hs --platform RNASeq
