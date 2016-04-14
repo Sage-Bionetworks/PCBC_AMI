c61c74e094af282799c7d51e4db82fb206d41db9
kdaily <kdaily@users.noreply.github.com>
2016-04-14 15:47:46 -0700

    added separate remote x provisioners

---
+++ x2go_provisioner.sh
@@ -0,0 +1,5 @@
+## x2go server for remote access
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:x2go/stable
+apt-get update
+apt-get install -y x2goserver x2goserver-xsession
