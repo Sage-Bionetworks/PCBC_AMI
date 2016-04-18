f52e4d976080aa8bc2eb4a9c277fae4d4ab1ad73
Kenneth Daily <kmdaily@gmail.com>
2016-04-18 16:08:50 +0000

    change ucsc tools path

--- bioinfo_paths.sh
+++ bioinfo_paths.sh
@@ -1,2 +1,5 @@
-export PATH=$PATH:/opt/altanalyze:/opt/homer/bin:/opt/ngsplot-2.61/bin:/opt/ucsc:/opt/weblogo
+export PATH=$PATH:/opt/altanalyze:/opt/homer/bin:/opt/ngsplot-2.61/bin:/opt/ucsc_tools/userApps/bin:/opt/weblogo
 export NGSPLOT=/opt/ngsplot-2.61
+alias startIGV='java -Xmx750m -jar /opt/IGV/IGV_2.3.72/igv.jar'
+
+
